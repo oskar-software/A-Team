496fac763e1a65aab6f33569892ace664a5e5dc5
KillerSTEFAnCO <31640274+KillerSTEFAnCO@users.noreply.github.com>
2020-04-27 20:47:38 +0100

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,14 @@
+//responsive navigation
+let hamburger = {
+    navToggle: document.querySelector('.nav-toggle'),
+    nav: document.querySelector('nav'),
+
+    doToggle: function(e) {
+        e.preventDefault();
+        this.navToggle.classList.toggle('expanded');
+        this.nav.classList.toggle('expanded');
+    }
+};
+hamburger.navToggle.addEventListener('click', (e) => {
+    hamburger.doToggle(e)
+});
